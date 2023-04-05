7471123e5734cb4ef855cd2c3b8d33d5d6633266
smichalski3 <smichalski0302@gmail.com>
2023-04-04 21:36:11 -0500

    activity 9

--- js/projection.js
+++ js/projection.js
@@ -1,11 +1,11 @@
 //begin script when window loads
 window.onload = setMap();
 
-//Example 1.3 line 4...set up choropleth map
+//set up choropleth map
 function setMap() {
     //map frame dimensions
     var width = 960,
-        height = 460;
+        height = 500;
 
     //create new svg container for the map
     var map = d3
@@ -15,21 +15,20 @@ function setMap() {
         .attr("width", width)
         .attr("height", height);
 
-    //create Albers equal area conic projection centered on France
+    //create Albers equal area conic projection centered on United States
     var projection = d3
-        .conicEqualArea()
+        .geoAlbers()
         .parallels([29.5, 45.5])
         .scale(1070)
         .translate([480, 250])
         .rotate([96, 0])
         .center([-0.6, 38.7])
-        .translate([width / 2, height / 2]);
 
     var path = d3.geoPath().projection(projection);
 
     //use Promise.all to parallelize asynchronous data loading
     var promises = [
-        d3.csv("data/statesberries.csv"),
+        d3.csv("data/stateberries.csv"),
         d3.json("data/northamerica.topojson"),
         d3.json("data/states.topojson"),
     ];
@@ -40,9 +39,9 @@ function setMap() {
             northamerica = data[1],
             states = data[2];
 
-    //translate europe TopoJSON
+    //translate north american TopoJSON
     var americanCountries = topojson.feature(northamerica, northamerica.objects.northamerica),
-        unitedStates = topojson.feature(states, states.objects.states).feautures;
+        unitedStates = topojson.feature(states, states.objects.states).features;
 
         var graticule = d3.geoGraticule().step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
@@ -62,21 +61,22 @@ function setMap() {
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
 
-        //add Europe countries to map
+        //add countries to map
         var countries = map
             .append("path")
             .datum(americanCountries)
             .attr("class", "countries")
             .attr("d", path);
 
-        //add France regions to map
+        //add states to map
         var regions = map
-            .selectAll(".regions")
+            .selectAll(".states")
             .data(unitedStates)
             .enter()
             .append("path")
+            .attr("class", "states")
             .attr("class", function (d) {
-                return "regions " + d.properties.name;
+                return "states" + d.properties.name;
             })
             .attr("d", path);
     }
